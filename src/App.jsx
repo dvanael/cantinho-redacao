1cd45c56ac0bbdc4794fb55df45f5197bebf2306
Ana Barbosa <anael.noki@gmail.com>
2025-03-02 16:54:06 -0300

    feat: adiciona projeto inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='title'>Cantinho da Redação</h1>
+    </>
+  )
+}
+
+export default App
